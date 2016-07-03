bbe464a8970362c26a9ecaa8777b398a3d5419ef
schrodog <sky_sun_tg@hotmail.com>
2016-07-03 19:19:48 +0800

    great improvement

--- DrawRegions.py
+++ DrawRegions.py
@@ -2,33 +2,24 @@ import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-class DrawRegions(QWidget):
+class DrawRegions(QGraphicsPixmapItem):
 
-	def __init__(self):
+	def __init__(self,pixmap=None,parent=None,scene=None):
 		super(DrawRegions,self).__init__()
-		self.initUI()
 		self.spaceArray=[]
 
-	def initUI(self):
-		self.setFixedSize(500,500)
-		self.setWindowTitle("DrawRegions")
-		self.show()
+	def paint(self,painter,options,widget=None):
+		painter.drawPixmap(0,0,self.pixmap())
 
-	def paintEvent(self,event):
-		paint=QPainter()
-		paint.begin(self)
-		self.drawSpace(paint)
-		paint.end()
+		radius=30
+		painter.setPen(Qt.NoPen)
+		painter.setBrush(QColor(255,0,0,125))
+		for coor in self.spaceArray:
+			painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
 
 	def keyPressEvent(self,event):
 		if event.key()==Qt.Key_Escape:
 			self.close()
-
-	def drawSpace(self,paint):
-		paint.setPen(Qt.NoPen)
-		paint.setBrush(QColor(255,0,0,125))
-		for coor in self.spaceArray:
-			paint.drawRect(coor[0],coor[1],12,12)
 
 	def mousePressEvent(self,event):
 		self.mouseClicking=True
@@ -40,7 +31,7 @@ class DrawRegions(QWidget):
 			self.y=event.pos().y()
 			if self.notRepeat(self.x,self.y):
 				self.spaceArray.insert(0,[self.x,self.y])
-				self.repaint()			
+				self.update()			
 
 	def mouseReleaseEvent(self,event):
 		self.mouseClicking=False
@@ -53,10 +44,43 @@ class DrawRegions(QWidget):
 				return False
 		return True
 
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super(MainWindow,self).__init__()
+
+		self.scene=QGraphicsScene()
+		self.scene.setSceneRect(0, 0, 800, 600)
+
+		pixmap=self.openImage()
+		self.imagePanel=DrawRegions(scene=self.scene)
+		self.imagePanel.setPixmap(pixmap)
+		self.scene.addItem(self.imagePanel)
+
+		self.view = QGraphicsView(self.scene)
+		layout=QHBoxLayout()
+		layout.addWidget(self.view)
+
+		self.widget=QWidget()
+		self.widget.setLayout(layout)
+
+		self.setCentralWidget(self.widget)
+		self.setWindowTitle("Draw Regions")
+
+	def openImage(self):
+		fname=QFileDialog.getOpenFileName(self,"Open image",
+			"/home/lkit/Pictures","Image Files (*.bmp *.jpg *.png *.xpm)")
+		if fname.isEmpty(): return None
+		return QPixmap(fname)
+
+
+
 def main():        
     app=QApplication(sys.argv)
-    ex=DrawRegions()
-    sys.exit(app.exec_())
+    ex=MainWindow()
+    ex.show()
+    app.exec_()
+    app.deleteLater()
+    sys.exit()
 
 if __name__=='__main__':
     main()		
