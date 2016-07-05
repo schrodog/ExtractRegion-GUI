8bb256aa17b8e5e54a1e5e8de68f5c9c355209f5
schrodog <sky_sun_tg@hotmail.com>
2016-07-05 16:53:42 +0800

    fix bugs

--- DrawRegions.py
+++ DrawRegions.py
@@ -1,4 +1,8 @@
 import sys
+import pyfits
+from PIL import Image
+import numpy as np
+from math import sqrt
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -6,16 +10,18 @@ class DrawRegions(QGraphicsPixmapItem):
 
 	def __init__(self,pixmap=None,parent=None,scene=None):
 		super(DrawRegions,self).__init__()
+		global graphX
+		global graphY
 		self.spaceArray, self.finalArray=[],[]
 		self.setFlags(QGraphicsPixmapItem.ItemIsFocusable)
 		self.scale=1.0
 		self.mouseLeftClicking, self.mouseRightClicking=False, False
-		# self.setTransformOriginPoint(self.pixmap().height()/2,self.pixmap().height()/2)
 		self.scaleX, self.scaleY = 0,0
 		self.brushSize=30
-		# self.setX(-250)
-		# self.setY(-50)
-
+		
+		print 'draw=',graphX,graphY
+		self.minX, self.maxX=graphX,0
+		self.minY, self.maxY=graphY,0
 
 	def paint(self,painter,options,widget=None):
 		painter.drawPixmap(0,0,self.pixmap())
@@ -23,20 +29,16 @@ class DrawRegions(QGraphicsPixmapItem):
 		radius=self.brushSize
 		painter.setPen(Qt.NoPen)
 		painter.setBrush(QColor(125,125,125,20))
-		if self.mouseLeftClicking:
-			for i in self.finalArray:
-				for coor in i:
-					radius=coor[2]
-					painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
-			for coor in self.spaceArray:
+
+		for i in self.finalArray:
+			for coor in i:
 				radius=coor[2]
 				painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
 
-		if not self.mouseLeftClicking:
-			for i in self.finalArray:
-				for coor in i:
-					radius=coor[2]
-					painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
+		if self.mouseLeftClicking:
+			for coor in self.spaceArray:
+				radius=coor[2]
+				painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
 
 	def mousePressEvent(self,event):
 		if event.button()==Qt.LeftButton:
@@ -46,8 +48,8 @@ class DrawRegions(QGraphicsPixmapItem):
 			# print event.pos().x(),event.pos().y()
 			self.scaleX=event.pos().x()
 			self.scaleY=event.pos().y()
-			self.setX(self.pixmap().width()/2-self.scaleX)
-			self.setY(self.pixmap().height()/2-self.scaleY)
+			self.setX(graphX/2-self.scaleX)
+			self.setY(graphY/2-self.scaleY)
 			
 		self.update()
 
@@ -58,6 +60,28 @@ class DrawRegions(QGraphicsPixmapItem):
 			r=self.brushSize
 			if self.notRepeat(self.x,self.y):
 				self.spaceArray.insert(0,[self.x,self.y,r])
+				if self.x+r>self.maxX:
+					if self.x+r <= graphX:
+						self.maxX=self.x+r
+					else: 
+						self.maxX=graphX
+				if self.x-r<self.minX:
+					if self.x-r >= 0:
+						self.minX=self.x-r
+					else:
+						self.minX=0
+
+				if self.y+r>self.maxY:
+					if self.y+r <= graphY:
+						self.maxY=self.y+r
+					else: 
+						self.maxY=graphY
+				if self.y-r<self.minY:
+					if self.y-r >= 0:
+						self.minY=self.y-r
+					else:
+						self.minY=0
+				print self.minX,self.maxX,self.minY,self.maxY,self.x,self.y
 				self.update()			
 
 	def mouseReleaseEvent(self,event):
@@ -105,57 +129,185 @@ class DrawRegions(QGraphicsPixmapItem):
 
 	def delArray(self):
 		self.finalArray = self.finalArray[1:]
-		self.update()		
+		self.update()
+	def loadArray(self):
+		return self.finalArray
+
+	def saveRegion(self):
+		fname=QFileDialog.getSaveFileName(None,"Save Region","/home/lkit/Pictures")
+		f=open(fname,'w')
+		f.write("%d %d\n" %(graphX,graphY))
+		f.write("%d %d %d %d\n" %(self.minX,self.maxX,self.minY,self.maxY))
+		for i in self.finalArray:
+			for j in i:
+				f.write("%d %d %d | " %(j[0], j[1], j[2]))
+			f.write("\n")
+		f.close()
+
+	def loadRegion(self):
+		fname=QFileDialog.getOpenFileName(None,"Load Region","/home/lkit/Pictures")
+		f=open(fname)
+		dimension=map(int,f.readline().split())
+		minX,maxX,minY,maxY=map(int,f.readline().split())
+		data=[[map(int,i.split()) for i in line.split("|") if i.split()] for line in f]
+		f.close()
+		if (graphX>=dimension[0] and graphY>=dimension[1]):
+			self.finalArray=data
+		else:
+			# errorMessage=QErrorMessage()
+			print 'mismatch'
+			# errorMessage.showMessage("Mismatched region!")
+	def loadComputeRegion(self):
+		data=[self.minX,self.maxX,self.minY,self.maxY]
+		return data
+
+
 
 class MainWindow(QMainWindow):
 	def __init__(self):
 		super(MainWindow,self).__init__()
+		global graphX
+		global graphY
+		print 'called mainWindow'
+		# graphX,graphY=self.pixmap.width(),self.pixmap.height()
 
-		self.scene=QGraphicsScene()
-		pixmap=self.openImage()
-		self.scene.setSceneRect(0, 0, pixmap.width(),pixmap.height())
-
-		self.imagePanel=DrawRegions(scene=self.scene)
-		self.imagePanel.setPixmap(pixmap)
-		self.scene.addItem(self.imagePanel)	#add GraphicsItem
-
-		self.view = QGraphicsView(self.scene)
-		layout=QHBoxLayout()
-		layout.addWidget(self.view)
-
-		self.widget=QWidget()
-		self.widget.setLayout(layout)
-
-		self.setCentralWidget(self.widget)
+		self.minX,self.maxX,self.minY,self.maxY=0,0,0,0
+		self.mode=0
 		self.setWindowTitle("Draw Regions")
 
 		self.toolBar=QToolBar()
 		self.addToolBar(Qt.LeftToolBarArea,self.toolBar)
-		# selectIcon=QIcon("/home/lkit/Pictures/drawing.png")
 		selectAction=self.toolBar.addAction("Select Mode")
 		# selectAction.setCheckable(True)
-		# maskIcon=QIcon("/home/lkit/Pictures/Xiao Hei.png")
 		maskAction=self.toolBar.addAction("Mask Mode")
 		undo=self.toolBar.addAction("undo")
 		self.toolBar.addSeparator()
+		newImageAction=self.toolBar.addAction("New image")
+		saveRegionAction=self.toolBar.addAction("Save Region")
+		loadRegionAction=self.toolBar.addAction("Load Region")		
+		self.toolBar.addSeparator()
+		exportAsFITSAction=self.toolBar.addAction("Export As FITS")
+		exportAsPNGAction=self.toolBar.addAction("Export As PNG")
 
 		self.statusBar=QStatusBar()
 		self.setStatusBar(self.statusBar)
-		
-		def showMsg01():
-			self.statusBar.showMessage("Select Mode")
-		def showMsg02():
-			self.statusBar.showMessage("Mask Mode")
-		def undoAction():
-			self.imagePanel.delArray()
 
-		selectAction.triggered.connect(showMsg01)
-		maskAction.triggered.connect(showMsg02)
-		undo.triggered.connect(undoAction)
+		selectAction.triggered.connect(self.showMsg01)
+		maskAction.triggered.connect(self.showMsg02)
+		undo.triggered.connect(self.undoAction)
+		newImageAction.triggered.connect(self.newImage)
+		saveRegionAction.triggered.connect(self.saveRegion)
+		loadRegionAction.triggered.connect(self.loadRegion)
+		exportAsFITSAction.triggered.connect(self.exportAsFits)
+		exportAsPNGAction.triggered.connect(self.exportAsPNG)
+
+		
+	def showMsg01(self):
+		self.mode=1
+		self.statusBar.showMessage("Select Mode")
+	def showMsg02(self):
+		self.mode=2
+		self.statusBar.showMessage("Mask Mode")
+	def undoAction(self):
+		self.imagePanel.delArray()
+	def saveRegion(self):
+		self.imagePanel.saveRegion()
+	def loadRegion(self):
+		self.imagePanel.loadRegion()
+	def newImage(self):
+		global graphX
+		global graphY
+		self.scene=QGraphicsScene()
+		self.pixmap=self.openImage()
+		graphX,graphY= self.pixmap.width(),self.pixmap.height()
+		print 'called newImage:',graphX,graphY
+		self.scene.setSceneRect(0, 0, graphX,graphY)
+		self.imagePanel=DrawRegions(scene=self.scene)
+		self.imagePanel.setPixmap(self.pixmap)
+		self.scene.addItem(self.imagePanel)	#add GraphicsItem
+		self.view = QGraphicsView(self.scene)
+		layout=QHBoxLayout()
+		layout.addWidget(self.view)
+		self.widget=QWidget()
+		self.widget.setLayout(layout)
+		self.setCentralWidget(self.widget)
+
+	def exportAsFits(self):
+		def dist(x,y,X,Y,R):
+		    return sqrt((x-X)**2+(y-Y)**2)<=R
+		# maxX, maxY=self.pixmap.width(),self.pixmap.height()
+		points=self.imagePanel.loadArray()
+		if self.mode != 0:
+			if self.mode==1:
+				TFtable=np.asarray([[False for i in range(maxX)] for j in range(maxY)])
+				mode_const=True
+			elif self.mode==2:
+				TFtable=np.asarray([[True for i in range(maxX)] for j in range(maxY)])
+				mode_const=False
+
+			for i in range(minX,maxX+1):
+				for j in range(minY,maxY+1):
+					for k0 in points:
+						for k in k0:
+							if dist(j,i,k[1],k[0],k[2]):
+								TFtable[j,i]=mode_const
+			input_fits=QFileDialog.getOpenFileName(self,"Open FITS image",
+				"/home/lkit/Data","*.fits")
+			f=pyfits.open(input_fits)
+			data=np.asarray(f[0].data)
+			f.close()
+			for i in range(maxX):
+				for j in range(maxY):
+					if not TFtable[j,i]:
+						data[j,i]=0
+
+			hdu=pyfits.PrimaryHDU(data)
+			out_fits=QFileDialog.getSaveFileName(None,"Save FITS image","/home/lkit/Pictures")
+			hdu.writeto(out_fits)
+		else:
+			print 'Please choose select/mask mode'
+
+	def exportAsPNG(self):
+		def dist(x,y,X,Y,R):
+		    return sqrt((x-X)**2+(y-Y)**2)<=R
+		self.minX,self.maxX,self.minY,self.maxY=self.imagePanel.loadComputeRegion()
+
+		points=np.asarray(self.imagePanel.loadArray())
+
+		if self.mode != 0:
+			if self.mode==1:
+				TFtable=np.asarray([[False for i in range(maxX)] for j in range(maxY)])
+				mode_const=True
+			elif self.mode==2:
+				TFtable=np.asarray([[True for i in range(maxX)] for j in range(maxY)])
+				mode_const=False
+
+			for i in range(minX,maxX):
+				for j in range(minY,maxY):
+					for k0 in points:
+						for k in k0:
+							if dist(j,i,k[1],k[0],k[2]):
+								TFtable[j,i]=mode_const
+				print 'i=',i
+			imInput=Image.open(self.input_image,mode='RGB')
+			input_data=np.asarray(imInput,dtype=np.uint8)
+
+			for i in range(minX,maxX):
+				for j in range(minY,maxY):
+					if not TFtable[j,i]:
+						input_data[j,i]=[0,0,0]
+			imOutput=Image.fromarray(input_data,mode='RGB')
+			out_image=QFileDialog.getSaveFileName(None,"Save PNG Image","/home/lkit/Pictures",
+				"Image Files (*.bmp *.jpg *.png *.xpm)")
+			imOutput.save(out_image)
+		else:
+			print 'Please choose select/mask mode'
+
 
 	def openImage(self):
 		fname=QFileDialog.getOpenFileName(self,"Open image",
 			"/home/lkit/Pictures","Image Files (*.bmp *.jpg *.png *.xpm)")
+		self.input_image=fname
 		if fname.isEmpty(): return None
 		return QPixmap(fname)
 
