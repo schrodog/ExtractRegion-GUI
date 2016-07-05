70a52e34fe4cf1ee9ced44884bd5fb45aeae468d
schrodog <sky_sun_tg@hotmail.com>
2016-07-05 10:00:41 +0800

    add few actions

--- DrawRegions.py
+++ DrawRegions.py
@@ -6,7 +6,7 @@ class DrawRegions(QGraphicsPixmapItem):
 
 	def __init__(self,pixmap=None,parent=None,scene=None):
 		super(DrawRegions,self).__init__()
-		self.spaceArray=[]
+		self.spaceArray, self.finalArray=[],[]
 		self.setFlags(QGraphicsPixmapItem.ItemIsFocusable)
 		self.scale=1.0
 		self.mouseLeftClicking, self.mouseRightClicking=False, False
@@ -23,16 +23,26 @@ class DrawRegions(QGraphicsPixmapItem):
 		radius=self.brushSize
 		painter.setPen(Qt.NoPen)
 		painter.setBrush(QColor(125,125,125,20))
-		for coor in self.spaceArray:
-			radius=coor[2]
-			painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
+		if self.mouseLeftClicking:
+			for i in self.finalArray:
+				for coor in i:
+					radius=coor[2]
+					painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
+			for coor in self.spaceArray:
+				radius=coor[2]
+				painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
+
+		if not self.mouseLeftClicking:
+			for i in self.finalArray:
+				for coor in i:
+					radius=coor[2]
+					painter.drawEllipse(coor[0]-radius,coor[1]-radius,2*radius,2*radius)
 
 	def mousePressEvent(self,event):
-		# print self.boundingRect()
 		if event.button()==Qt.LeftButton:
 			self.mouseLeftClicking=True
+			self.spaceArray=[]
 		elif event.button()==Qt.RightButton:
-			# self.mouseRightClicking=True
 			# print event.pos().x(),event.pos().y()
 			self.scaleX=event.pos().x()
 			self.scaleY=event.pos().y()
@@ -53,8 +63,8 @@ class DrawRegions(QGraphicsPixmapItem):
 	def mouseReleaseEvent(self,event):
 		self.mouseLeftClicking=False
 		self.mouseRightClicking=False
+		self.finalArray.insert(0,self.spaceArray)
 		self.update()
-		print self.spaceArray
 
 	def keyPressEvent(self,event):
 		super(DrawRegions,self).keyPressEvent(event) 	#need this,can press esc
@@ -83,6 +93,8 @@ class DrawRegions(QGraphicsPixmapItem):
 				self.brushSize -= 5
 			elif self.brushSize <=5 and self.brushSize>1:
 				self.brushSize -= 1
+		elif event.key()==Qt.Key_U:
+			self.delArray()
 
 
 	def notRepeat(self,x,y):
@@ -91,18 +103,21 @@ class DrawRegions(QGraphicsPixmapItem):
 				return False
 		return True
 
+	def delArray(self):
+		self.finalArray = self.finalArray[1:]
+		self.update()		
+
 class MainWindow(QMainWindow):
 	def __init__(self):
 		super(MainWindow,self).__init__()
 
 		self.scene=QGraphicsScene()
 		pixmap=self.openImage()
-
 		self.scene.setSceneRect(0, 0, pixmap.width(),pixmap.height())
 
 		self.imagePanel=DrawRegions(scene=self.scene)
 		self.imagePanel.setPixmap(pixmap)
-		self.scene.addItem(self.imagePanel)
+		self.scene.addItem(self.imagePanel)	#add GraphicsItem
 
 		self.view = QGraphicsView(self.scene)
 		layout=QHBoxLayout()
@@ -113,6 +128,30 @@ class MainWindow(QMainWindow):
 
 		self.setCentralWidget(self.widget)
 		self.setWindowTitle("Draw Regions")
+
+		self.toolBar=QToolBar()
+		self.addToolBar(Qt.LeftToolBarArea,self.toolBar)
+		# selectIcon=QIcon("/home/lkit/Pictures/drawing.png")
+		selectAction=self.toolBar.addAction("Select Mode")
+		# selectAction.setCheckable(True)
+		# maskIcon=QIcon("/home/lkit/Pictures/Xiao Hei.png")
+		maskAction=self.toolBar.addAction("Mask Mode")
+		undo=self.toolBar.addAction("undo")
+		self.toolBar.addSeparator()
+
+		self.statusBar=QStatusBar()
+		self.setStatusBar(self.statusBar)
+		
+		def showMsg01():
+			self.statusBar.showMessage("Select Mode")
+		def showMsg02():
+			self.statusBar.showMessage("Mask Mode")
+		def undoAction():
+			self.imagePanel.delArray()
+
+		selectAction.triggered.connect(showMsg01)
+		maskAction.triggered.connect(showMsg02)
+		undo.triggered.connect(undoAction)
 
 	def openImage(self):
 		fname=QFileDialog.getOpenFileName(self,"Open image",
@@ -129,7 +168,7 @@ def main():
     ex=MainWindow()
     ex.show()
     app.exec_()
-    app.deleteLater()
+    app.deleteLater() 	# need this avoid error
     sys.exit()
 
 if __name__=='__main__':
